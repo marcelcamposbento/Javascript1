c95e87b4940699c3d86e5cd25a4cd38c9b775554
marcelcamposbento <142677314+marcelcamposbento@users.noreply.github.com>
2024-09-20 17:04:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+console.log(5 > 2 && 3 == 2);
+console.log(5 > 3 || 3 == 1);
+console.log(3 == 3 && 'marcel' == 'marcel');
+console.log('marcel' == 'jackson' || false);
+console.log(!(!(true && true)));
+console.log(true && true);
+console.log(false || false);
+
